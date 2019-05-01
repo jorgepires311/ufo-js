0093f8d0db131e5cd0fade9ac6b20cb513a7a1f2
Jorge Pires <jorgepires311@gmail.com>
2019-04-30 21:17:30 -0700

    Adding completed step 2

--- static/js/app.js
+++ static/js/app.js
@@ -20,10 +20,28 @@ var submit = d3.select("#filter-btn");
     // Prevent the page from refreshing
     d3.event.preventDefault();
     // Select the input element and get the raw HTML node
-    var inElement = d3.select('#datetime');
+    var inElement = d3.select('#searchVal');
+    var qType = d3.select('#queryType').property("value");
     // Get the value property of the input element
     var inElementVal = inElement.property("value");
-    var filterData = tableData.filter(dt => dt.datetime === inElementVal);
+    var filterData;
+    switch(qType){
+        case "date":
+            filterData = tableData.filter(q => q.datetime === inElementVal);
+            break;
+        case "city":
+            filterData = tableData.filter(q => q.city === inElementVal);
+            break;
+        case "state":
+            filterData = tableData.filter(q => q.state === inElementVal);
+            break;
+        case "country":
+            filterData = tableata.filter(q => q.country === inElementVal);
+            break;
+        case "shape":
+            filterData = tableData.filter(q => q.shape === inElementVal);
+            break;
+    }
     d3.selectAll("tbody tr").remove();
     filterData.forEach(d=>{
         row =tbody.append('tr');
